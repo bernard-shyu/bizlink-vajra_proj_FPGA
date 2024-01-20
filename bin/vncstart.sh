9c169b82dc7c6f2b2aa62d594efd8ffd43de2e52
Bernard Shyu <bernard.shyu@gmail.com>
2024-01-20 09:50:42 +0800

    TightVNC installation #2 (OK)
    New vncstart.sh script to launch VNC Server <vncserver> with
    env: VNC_GEOMETRY for different resolution.
    
    Modified .vnc/xstartup to work under the current Ubuntu 22.04
    installation. This script will be invoked from <vncserver> to setup
    $USER home X-Server environment.

---
+++ bin/vncstart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#----------------------------------------------------------------------------
+echo "kill previous VNC Server instances"
+for i in `seq 1 9`; do 
+	vncserver -kill :$i               > /dev/null  2>&1
+#	sudo rm -f /tmp/.X$i-lock         > /dev/null  2>&1
+#	sudo rm -f /tmp/.X11-unix/X$i     > /dev/null  2>&1
+done
+
+# Common geometry: 1280x800 / 1920x1080 / 2048x1280 / 2560x1440
+VNC_GEOMETRY=${VNC_GEOMETRY:=1280x800}      # externally defined export VNC_GEOMETRY
+VNC_GEOMETRY=${1:-$VNC_GEOMETRY}            # input parameter %1 for VNC_GEOMETRY
+VNC_LOCAL=${VNC_LOCAL:=""}                  # VNC_LOCAL="-localhost" or ""
+VNC_PORT=${VNC_PORT:="9"}                   # VNC_PORT: 1 ~ 15
+
+cd $HOME
+vncserver :$VNC_PORT $VNC_LOCAL -depth 24 -geometry $VNC_GEOMETRY
