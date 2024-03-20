cb43b501ae8d3143c500fb9c58344a871f209e28
Bernard Shyu <bernard_shyu@bizlinktech.com>
2024-03-20 10:15:37 +0800

    [FPGA] improve bxu_vpk120_ibert.tcl
    TCL_scripts/bxu_vpk120_ibert.tcl: improve script
    - Adding comments
    - Taking optional 4th arguments, to {open_project} / {program_hw_devices} or not
    
    Minor modifying ChipScoPy scripts in ChipScoPy/my_learning/
    - Still only <versal_gtm> can work on VPK120.
      Modify <versal_gty> & <versal_gtyp> to get closer to VPK120

--- TCL_scripts/bxu_vpk120_ibert.tcl
+++ TCL_scripts/bxu_vpk120_ibert.tcl
@@ -1,49 +1,95 @@
-if { $argc != 3 } {
-	puts "Syntax:  $argv0  <PROJ_PATH>  <PROJ_NAME>  <HW_ID: 111 or 112 or -111 or -112 >"
+#-------------------------------------------------------------------------------------------------------
+# Script execution: 
+#	TCL shell:    TCL_Vivado.sh -source ~/fpgaspace/TCL_scripts/bxu_vpk120_ibert.tcl  -tclargs $HOME/VIVADO_projects/iBERT_study bxu_vpk120_ibert_2xQDD_56G 111 PI
+#	TCL_Console%  set BXU_HW_ID 111
+#	TCL_Console%  set argc 3; set argv [list $env(HOME)/VIVADO_projects/iBERT_study bxu_vpk120_ibert_2xQDD_56G $BXU_HW_ID];   source $env(HOME)/fpgaspace/TCL_scripts/bxu_vpk120_ibert.tcl 
+#	TCL_Console%  set argc 4; set argv [list $env(HOME)/VIVADO_projects/iBERT_study bxu_vpk120_ibert_2xQDD_56G $BXU_HW_ID I]; source $env(HOME)/fpgaspace/TCL_scripts/bxu_vpk120_ibert.tcl 
+#-------------------------------------------------------------------------------------------------------
+if { $argc < 3 } {
+	puts "Syntax:  $argv0  <PROJ_PATH>  <PROJ_NAME>  <HW_ID: 111 / 112>  [ <options>: P (open project) / I (program Image) ]"
 	puts "Example: $argv0  /home/bernard/VIVADO_projects/iBERT_study bxu_vpk120_ibert_2xQDD_56G 111"
 	exit
 } else {
 	set BXU_PROJ_PATH [lindex $argv 0]
 	set BXU_PROJ_NAME [lindex $argv 1]
-	set BXU_HW_ID [lindex $argv 2]
+	set BXU_HW_ID     [lindex $argv 2]
+
+	set BXU_OPTIONS ""
+	set BXU_OPEN_PROJ   0
+	set BXU_PROG_IMAGE  0
+	if { $argc > 3 } {
+		set BXU_OPTIONS [lindex $argv 3]
+		switch -regexp -- $BXU_OPTIONS {
+		  PI|IP { set BXU_OPEN_PROJ 1; set BXU_PROG_IMAGE 1; }
+		  P     { set BXU_OPEN_PROJ 1 }
+		  I     { set BXU_PROG_IMAGE 1; }
+		}
+	}
 }
 
+set TCL_SCRIPT_PATH [ exec dirname $argv0 ]
 set BXU_PROJ_FILE  ${BXU_PROJ_PATH}/${BXU_PROJ_NAME}/${BXU_PROJ_NAME}.xpr
 set BXU_IMAGE_FILE [ exec find ${BXU_PROJ_PATH}/${BXU_PROJ_NAME}/${BXU_PROJ_NAME}.runs -name \*.pdi ]
 set BXU_BOARD_FILE [ exec find ${BXU_PROJ_PATH}/${BXU_PROJ_NAME}/${BXU_PROJ_NAME}.srcs -name \*.bd ]
 
-puts "Run as: $argv0 $BXU_PROJ_PATH $BXU_PROJ_NAME $BXU_HW_ID"
+puts "Run as: $argv0 $BXU_PROJ_PATH $BXU_PROJ_NAME $BXU_HW_ID $BXU_OPTIONS"
 puts "Vivado Project: $BXU_PROJ_FILE"
 puts "Vivado Image:   $BXU_IMAGE_FILE"
 puts "Vivado Board:   $BXU_BOARD_FILE"
+puts "open_Project: $BXU_OPEN_PROJ,  program_Image: $BXU_PROG_IMAGE"
 
-if { $BXU_HW_ID > 0 } {
+#-------------------------------------------------------------------------------------------------------
+# Open Vivado Project file
+#-------------------------------------------------------------------------------------------------------
+if { $BXU_OPEN_PROJ } {
 	start_gui
 	open_project $BXU_PROJ_FILE 
 	update_compile_order -fileset sources_1
-} else {
-	set BXU_HW_ID [ expr 0 - $BXU_HW_ID ]
 }
 
+#-------------------------------------------------------------------------------------------------------
+# Open Vivado Board Design file
+#-------------------------------------------------------------------------------------------------------
 # open_bd_design $BXU_BOARD_FILE
 
+# TCL for FPGA Hardware check:
+#	current_hw_server  ==> TPLab-Ubuntu-1:3121
+#	current_hw_target  ==> TPLab-Ubuntu-1:3121/xilinx_tcf/Xilinx/872311160111A
+#	current_hw_device  ==> xcvp1202_1
+
+#-------------------------------------------------------------------------------------------------------
+# Open Hardware Server -- daily disconnect / re-connect to FPGA hw_server
+#-------------------------------------------------------------------------------------------------------
 open_hw_manager
-if { $BXU_HW_ID == 111 } {
-	connect_hw_server -url TPLab-Ubuntu-1:3121 -allow_non_jtag
-	current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/872311160111A]
-	set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/872311160111A]
-} else {
-	connect_hw_server -url HPElite-i7-TP261:3121 -allow_non_jtag
-	current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/872311160112A]
-	set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/872311160112A]
+
+set CurrHW [current_hw_server];         # daily disconnect / re-connect to FPGA hw_server
+puts "current_hw_server: $CurrHW"
+if { $CurrHW != "" }  { disconnect_hw_server   "$CurrHW" }
+
+switch $BXU_HW_ID {
+  111 { set CurrHW "TPLab-Ubuntu-1:3121" }
+  112 { set CurrHW "HPElite-i7-TP261:3121" }
 }
+
+connect_hw_server -url "$CurrHW" -allow_non_jtag
+
+#-------------------------------------------------------------------------------------------------------
+# Open Hardware Target / Program Device
+#-------------------------------------------------------------------------------------------------------
+current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/*]
+set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/*]
 open_hw_target
 
-set_property PROBES.FILE {} [get_hw_devices xcvp1202_1]
-set_property FULL_PROBES.FILE {} [get_hw_devices xcvp1202_1]
-set_property PROGRAM.FILE $BXU_IMAGE_FILE [get_hw_devices xcvp1202_1]
-program_hw_devices [get_hw_devices xcvp1202_1]
+if { $BXU_PROG_IMAGE } {
+	set_property PROBES.FILE {} [get_hw_devices xcvp1202_1]
+	set_property FULL_PROBES.FILE {} [get_hw_devices xcvp1202_1]
+	set_property PROGRAM.FILE $BXU_IMAGE_FILE [get_hw_devices xcvp1202_1]
+	program_hw_devices [get_hw_devices xcvp1202_1]
+}
+
 refresh_hw_device [lindex [get_hw_devices xcvp1202_1] 0]
 
-set TCL_SCRIPT_PATH [ exec dirname $argv0 ]
+#-------------------------------------------------------------------------------------------------------
+# Create iBERT Links for VPK120 QSP-DD channels
+#-------------------------------------------------------------------------------------------------------
 source ${TCL_SCRIPT_PATH}/vpk120_ibert_Channel-mapping.tcl
