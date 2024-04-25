cd689d984d9843ec017fbdc73a7ceaf615ef5574
Bernard Shyu <bernard_shyu@bizlinktech.com>
2024-04-25 11:32:18 +0800

    [SCOPY] 16x iBERT-Links & matplotlib/PyQT works
    @@ Support x16 rendering @@
    - new class MplCanvas(FigureCanvas): to associate with QGridLayout by below
      self.layout_grid.addWidget(canvas, row, col)
    
    - MyWidget::canvases[] stores all the MplCanvas objects (x16)
    - MyWidget::__init__(n_links) will determine the QGridLayout dimension: nROW x nCOL
    
    - MyWidget::layout_main = QVBoxLayout, as the central_widget. On this, laying out
      1) self.layout_TBar, QHBoxLayout object, place-holder for ToolBar. Empty now.
      2) self.layout_grid, main place for x16 iBERT figures rendering
      3) self.tableWidget, QTableWidget object, table for each link's iBERT values. No function now.
    
    - MyWidget::create_YKScan_figure() to associate each iBERT link with MplCanvas respectively
    
    @@ Support dual mode of x1 or x16 rendering @@
    - Currently x1 mode is determined by env(CREATE_LGROUP) being False. In this case,
      simulate the link with class FakeYKLink
    
    @@ Other changes @@
    - new env(PDI_FILE), accompanying with PROG_DEVICE to program the FPGA image
    - in create_links_common(), use RegEx to parse the link.rx into GT-Quad name and channel
            "RX=IBERT_0.Quad_204.CH_3.RX(RX)"  ==>  link.gt_name="Quad_204" / link.channel=3

--- ChipScoPy/HPC_Cable_BERT_YK_Scan.py
+++ ChipScoPy/HPC_Cable_BERT_YK_Scan.py
@@ -34,8 +34,9 @@
 #------------------------------------------
 """
 export ip="10.20.2.146";     export CS_SERVER_URL="TCP:$ip:3042" HW_SERVER_URL="TCP:$ip:3121"
-export HW_PLATFORM="vpk120"; export PROG_DEVICE=False; export CREATE_LGROUP=False
-export RESOLUTION_X=900;     export RESOLUTION_Y=800; 
+export HW_PLATFORM="vpk120"; export CREATE_LGROUP=False
+export PLOT_1_RESOL_X=900;   export PLOT_1_RESOL_Y=800;  export PLOT_F_RESOL_X=3840;  export PLOT_F_RESOL_Y=2160
+export PROG_DEVICE=True;     export PDI_FILE="./VPK120_iBERT_2xQDD_56G.pdi"
 export SHOW_FIG_TITLE=True;  export MAX_SLICES=100;
 export QUAD_NAME="Quad_202"; export QUAD_CHAN=2;
 """
@@ -64,6 +65,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 import sys
 import random
 import math
+import re
 import numpy as np
 
 import matplotlib
@@ -83,8 +85,11 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 # Specify locations of the running hw_server and cs_server below.
 #------------------------------------------------------------------------------------------
 
-RESOLUTION_X = int(os.getenv("RESOLUTION_X", "900"))
-RESOLUTION_Y = int(os.getenv("RESOLUTION_Y", "800"))
+PLOT_1_RESOL_X = int(os.getenv("PLOT_1_RESOL_X", "900"))
+PLOT_1_RESOL_Y = int(os.getenv("PLOT_1_RESOL_Y", "800"))
+PLOT_F_RESOL_X = int(os.getenv("PLOT_F_RESOL_X", "3200"))
+PLOT_F_RESOL_Y = int(os.getenv("PLOT_F_RESOL_Y", "1900"))
+
 QUAD_NAME    =     os.getenv("QUAD_NAME", "Quad_202")
 QUAD_CHAN    = int(os.getenv("QUAD_CHAN", "0"))
 
@@ -103,6 +108,7 @@ SHOW_FIG_TITLE = os.getenv("SHOW_FIG_TITLE", 'False').lower() in ('true', '1', '
 # configurations, you can put the path for PROGRAMMING_FILE and PROBES_FILE below.
 design_files = get_design_files(f"{HW_PLATFORM}/production/chipscopy_ced")
 PDI_FILE = design_files.programming_file
+PDI_FILE = os.getenv("PDI_FILE", "./VPK120_iBERT_2xQDD_56G.pdi")
 
 print(f"PROGRAMMING_FILE: {PDI_FILE}")
 print(f"Servers URL: {CS_URL} {HW_URL} HW: {HW_PLATFORM}  Do_Programming: {PROG_DEVICE}\n")
@@ -169,10 +175,18 @@ print(f"--> GT Groups available - {ibert_gtm.gt_groups}")
 
 
 #------------------------------------------------------------------------------------------
+class FakeYKLink():
+    def __init__(self):
+        self.status      = "53.109 Gbps"
+        self.line_rate   = "53.121 Gbps"
+        self.bit_count   = 0
+        self.error_count = 0
+        self.ber         = random.random() / 1000000
+
 class MyYKScan():
     def __init__(self, gt_group, gt_chan):
         self.YKName = f"YK-{gt_group.name}[{gt_chan}]"
-        self.fig = plt.figure(FigureClass=MyYKFigure, layout='constrained', num=self.YKName, figsize=[12,10])
+        self.fig = plt.figure(FigureClass=MyYKFigure, layout='constrained', num=self.YKName, figsize=[8,6])   # figsize=[12,10]  dpi=100
         self.fig.init_YK_axes(self.YKName)
 
         self.RX  = gt_group.gts[gt_chan].rx
@@ -180,7 +194,9 @@ class MyYKScan():
         self.PLL = gt_group.gts[gt_chan].pll
         self.YK  = create_yk_scans(target_objs=gt_group.gts[gt_chan].rx)[0]
         self.YK.updates_callback = lambda obj: self.yk_scan_updates(obj)
-        self.link  = self.RX.link
+
+        if CREATE_LGROUP:   self.link = self.RX.link
+        else:               self.link = FakeYKLink()
         self.ber   = self.link.ber
 
         #------------------------------------------------------------------------------
@@ -198,8 +214,8 @@ class MyYKScan():
         self.error_count = self.link.error_count
         self.ber         = self.link.ber                                                                                      # main BER read method: works
         #self.ber1       = self.link.rx.property_for_alias(RX_BER)                                                            # another BER method 1: not working
-        self.ber2        = list(self.link.rx.property.refresh(self.link.rx.property_for_alias[RX_BER]).values())[0]           # another BER method 2: works, almost the same value as <self.link.ber>
-        print(f"SELF={self}  LINK='{self.link}' values=({self.ber}, {self.ber2}, {self.status}, {self.line_rate}, {self.bit_count}, {self.error_count}) ")
+        #self.ber2       = list(self.link.rx.property.refresh(self.link.rx.property_for_alias[RX_BER]).values())[0]           # another BER method 2: works, almost the same value as <self.link.ber>
+        print(f"SELF={self}  LINK='{self.link}' values=({self.ber}, {self.status}, {self.line_rate}, {self.bit_count}, {self.error_count}) ")
 
     # ## 6 - Define YK Scan Update Method
     def yk_scan_updates(self, obj):
@@ -248,6 +264,7 @@ class MyYKFigure(matplotlib.figure.Figure):
         self.scatter_X_data  = np.linspace( 0, X_max - 1, X_max )
 
         # Each figure corresponds to a QUAD channel
+        self.fig_name = fig_name
         self.suptitle(fig_name)
 
         # axis of EYE diagram
@@ -258,9 +275,10 @@ class MyYKFigure(matplotlib.figure.Figure):
         self.ax_EYE.set_ylim(0,100)
         self.ax_EYE.set_yticks(range(0, 100, 20))
         if SHOW_FIG_TITLE: self.ax_EYE.set_title("Slicer eye")
+        else:              self.ax_EYE.set_xlabel("EYE")
 
         # Set custom x-axis labels (divide by 2000)
-        scatter_X_ticks  = self.scatter_X_data[0::int(X_max/10)]
+        scatter_X_ticks  = self.scatter_X_data[0::int(X_max/5)]
         scatter_X_labels = [f"{x/self.YKSCAN_SLICER_SIZE:.0f}" for x in scatter_X_ticks]
         self.ax_EYE.set_xticks(scatter_X_ticks, scatter_X_labels)
         self.scatter_plot_EYE = self.ax_EYE.scatter([], [], s=1, color='blue')
@@ -273,6 +291,7 @@ class MyYKFigure(matplotlib.figure.Figure):
         self.ax_HIST.set_ylim(0,100)
         self.ax_HIST.set_yticks(range(0, 100, 20))
         if SHOW_FIG_TITLE: self.ax_HIST.set_title("Histogram")
+        else:              self.ax_HIST.set_xlabel("Histogram")
 
         # axis of SNR diagram
         self.ax_SNR = plt.subplot(3,2,5)
@@ -281,6 +300,7 @@ class MyYKFigure(matplotlib.figure.Figure):
         self.ax_SNR.set_xlim(0,10)
         self.ax_SNR.set_ylim(-10,50)
         if SHOW_FIG_TITLE: self.ax_SNR.set_title("Signal-to-Noise Ratio")
+        else:              self.ax_SNR.set_xlabel("SNR")
 
         # axis of BER diagram
         self.ax_BER = plt.subplot(3,2,6)
@@ -289,6 +309,7 @@ class MyYKFigure(matplotlib.figure.Figure):
         self.ax_BER.set_xlim(0,10)
         self.ax_BER.set_ylim(-1,-20)
         if SHOW_FIG_TITLE: self.ax_BER.set_title("Bit-Error-Rate")
+        else:              self.ax_BER.set_xlabel("BER")
 
     # ## 6 - Define YK Scan Update Method
     # This method will be called each time the yk scan updates, allowing it to update its graphs in real time. 
@@ -306,10 +327,12 @@ class MyYKFigure(matplotlib.figure.Figure):
             self.axis_X_OFFSET += self.YKSCAN_SLICER_SIZE
 
         # Update the scatter plot with data from the buffer.
-        print(f"BXU: {self.axis_X_Count}, {self.scatter_X_data[0:self.axis_X_OFFSET].shape}, {self.YKScan_slicer_buf.flatten().shape}")
         self.scatter_plot_EYE.set_offsets(np.column_stack((self.scatter_X_data[0:self.axis_X_OFFSET], self.YKScan_slicer_buf.flatten())))  # Set new data points
 
-        self.ax_EYE.set_xlabel("ES Sample ({}): ({:.1f}, {:.1f}, {:.1f})".format(self.axis_X_Count, obj.scan_data[-1].slicer[-1], obj.scan_data[-1].slicer[-2], obj.scan_data[-1].slicer[-3]))
+        print("{}: samples#{:5d}  SHAPE: {} / {}   \t\tDATA: ({:.1f}, {:.1f}, {:.1f}, {:.1f})".format( self.fig_name,
+           self.axis_X_Count, self.scatter_X_data[0:self.axis_X_OFFSET].shape, self.YKScan_slicer_buf.shape,
+           obj.scan_data[-1].slicer[-1], obj.scan_data[-1].slicer[-2], obj.scan_data[-1].slicer[-3], obj.scan_data[-1].slicer[-4]))
+        #self.ax_EYE.set_xlabel("ES Sample ({}): ({:.1f}, {:.1f}, {:.1f})".format(self.axis_X_Count, obj.scan_data[-1].slicer[-1], obj.scan_data[-1].slicer[-2], obj.scan_data[-1].slicer[-3]))
 
         if self.ax_HIST.lines:
             for line2 in self.ax_HIST.lines:
@@ -318,7 +341,8 @@ class MyYKFigure(matplotlib.figure.Figure):
                 line2.set_ydata(list(line2.get_ydata()) + list(obj.scan_data[-1].slicer))
         else:
             #self.ax_HIST.cla()
-            self.ax_HIST.hist(list(obj.scan_data[-1].slicer), 50, orientation = 'horizontal', color='blue', stacked=True, range=(0,100))
+            #color: blue / green / teal / brown / charcoal / black / gray / silver / cyan / violet
+            self.ax_HIST.hist(list(obj.scan_data[-1].slicer), 50, orientation = 'horizontal', color='cyan', stacked=True, range=(0,100))
 
         if self.ax_SNR.lines:
             for line3 in self.ax_SNR.lines:
@@ -327,7 +351,7 @@ class MyYKFigure(matplotlib.figure.Figure):
                 val_snr =  obj.scan_data[-1].snr
                 line3.set_xdata(list(line3.get_xdata()) + [self.axis_X_Count])
                 line3.set_ydata(list(line3.get_ydata()) + [val_snr])
-                self.ax_SNR.set_xlabel(f"SNR Sample: {val_snr:.3f}")
+                #self.ax_SNR.set_xlabel(f"SNR Sample: {val_snr:.3f}")
         else:
             self.ax_SNR.plot(self.axis_X_Count, obj.scan_data[-1].snr)
 
@@ -339,49 +363,107 @@ class MyYKFigure(matplotlib.figure.Figure):
                     self.ax_BER.set_xlim(0, self.axis_X_Count+10)
                 line4.set_xdata(list(line4.get_xdata()) + [self.axis_X_Count])
                 line4.set_ydata(list(line4.get_ydata()) + [math.log10(ber)])
-                self.ax_BER.set_xlabel(f"BER Sample: {ber:.3E}")
+                #self.ax_BER.set_xlabel(f"BER Sample: {ber:.3E}")
         else:
-            self.ax_BER.plot(self.axis_X_Count, math.log10(ber))
+            self.ax_BER.plot(self.axis_X_Count, math.log10(ber), color='violet')
+
+
+#------------------------------------------
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, ykobj=None):
+        super().__init__(ykobj.fig)
+        self.setParent(parent)
+        self.ykobj = ykobj
+
+        # Setup a timer to trigger the redraw by calling update_plot.
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(5000)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        self.flush_events()
+        self.ykobj.yk_link_updates()
+        self.draw_idle()
 
 
 #------------------------------------------
 class MyWidget(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, n_links):
         super().__init__()
         self.setWindowTitle('BizLink HPC Cable Test')
-        self.resize(RESOLUTION_X, RESOLUTION_Y)
-        self.t = 0
 
-        # Setup a timer to trigger the redraw by calling update_plot.
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(2000)
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start()
-        self.redraw_slot = False
+        self.canvases = []
+        self.grid_row = 0
+        self.grid_col = 0
+        self.n_links  = n_links
+        if   n_links > 8:  self.layout_grid_rows = 2; self.layout_grid_cols = 8;  self.setGeometry(0, 0, PLOT_F_RESOL_X, PLOT_F_RESOL_Y)
+        elif n_links > 1:  self.layout_grid_rows = 2; self.layout_grid_cols = 4;  self.setGeometry(0, 0, PLOT_F_RESOL_X, PLOT_F_RESOL_Y)
+        else:              self.layout_grid_rows = 1; self.layout_grid_cols = 1;  self.resize(PLOT_1_RESOL_X, PLOT_1_RESOL_Y)
 
-    def update_plot(self):
-        self.redraw_slot = True
-        self.yk_obj.yk_link_updates()
-        self.canvas.draw_idle()
+        # Create a central widget
+        central_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        # Create a vertical layout for the central widget
+        self.layout_main = QtWidgets.QVBoxLayout(central_widget)
+
+        # Create a horizontal layout for the toobar
+        self.layout_TBar = QtWidgets.QHBoxLayout()
+        self.layout_main.addLayout(self.layout_TBar)
+
+        # Create a grid layout for the canvases
+        self.layout_grid = QtWidgets.QGridLayout()
+        self.layout_main.addLayout(self.layout_grid)
+
+        # Create iBERT multiple channels table 
+        self.createTable() 
+        self.layout_main.addWidget(self.tableWidget)
 
     def closeEvent(self, event):
         print("Close Widget & YK-Scan")
-        self.yk_obj.YK.stop() # Stops the YK scan from running.
+        for c in self.canvases:
+            c.ykobj.YK.stop() # Stops the YK scan from running.
         event.accept()  # Close the widget
 
+    def show_figures(self): 
+        for c in self.canvases:
+            #c.draw()
+            c.ykobj.yk_link_updates()
+            c.ykobj.YK.start()
+        self.show()
+
+    def createTable(self): 
+        self.tableWidget = QtWidgets.QTableWidget(self.n_links, 10) 
+
+        # Table will fit the screen horizontally 
+        self.tableWidget.setHorizontalHeaderLabels( ("Samples", "TX", "RX", "Status", "Bits", "Errors", "BER", "SNR", "NA", "NA") )
+        self.tableWidget.horizontalHeader().setStretchLastSection(True) 
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+
     def create_YKScan_figure(self, gt_group, gt_chan):
-        self.yk_obj = MyYKScan(gt_group, gt_chan)
-        self.ui2(self.yk_obj.fig)
-        self.yk_obj.YK.start()
+        ykobj  = MyYKScan(gt_group, gt_chan)
+        canvas = MplCanvas(self, ykobj)
+        self.layout_grid.addWidget(canvas, self.grid_row, self.grid_col)
+        self.canvases.append(canvas)
+        canvas.draw()
+
+        #self.ui2(self.ykobj.fig)
+        #ykobj.YK.start()
+
+        self.grid_col += 1
+        if  self.grid_col >= self.layout_grid_cols:
+            self.grid_col = 0
+            self.grid_row += 1
 
     def ui(self, fig):
         self.canvas = FigureCanvas(fig)
 
         self.graphicview = QtWidgets.QGraphicsView(self)
-        self.graphicview.setGeometry(0, 0, RESOLUTION_X, RESOLUTION_Y)
+        self.graphicview.setGeometry(0, 0, PLOT_1_RESOL_X, PLOT_1_RESOL_Y)
 
         self.graphicscene = QtWidgets.QGraphicsScene()
-        self.graphicscene.setSceneRect(0, 0, RESOLUTION_X - 20, RESOLUTION_Y - 20)
+        self.graphicscene.setSceneRect(0, 0, PLOT_1_RESOL_X - 20, PLOT_1_RESOL_Y - 20)
         self.graphicscene.addWidget(self.canvas)
 
         self.graphicview.setScene(self.graphicscene)
@@ -412,7 +494,10 @@ def create_links_common(RXs, TXs):
 
     dbg_print = True
     for link in myLinks:
-        print(f"\n----- {link.name} :: RX={link.rx} TX={link.tx} -------")
+        link.gt_name = re.findall(".*(Quad_[0-9]*).*", str(link.rx))[0]
+        link.channel = int(re.findall(".*CH_([0-9]*).*", str(link.rx))[0])
+        link.gt_grp  = ibert_gtm.gt_groups.filter_by(name=link.gt_name)[0]
+        print(f"\n----- {link.name} :: RX={link.rx} TX={link.tx}  GT={link.gt_name} CH={link.channel} -------")
         _, tx_pattern_report = link.tx.property.report(link.tx.property_for_alias[PATTERN]).popitem()
         _, rx_pattern_report = link.rx.property.report(link.rx.property_for_alias[PATTERN]).popitem()
         _, rx_loopback_report = link.tx.property.report(
@@ -422,8 +507,10 @@ def create_links_common(RXs, TXs):
         if dbg_print:
             print(f"--> Valid values for TX pattern - {tx_pattern_report['Valid values']}")
             print(f"--> Valid values for RX pattern - {rx_pattern_report['Valid values']}")
-            print(f"--> Valid values for RX loopback - {rx_loopback_report['Valid values']}")
-            print(f"\nlink.RX:  {link.rx} / {link.rx.parent} \tlink.TX:  {link.tx} / {link.tx.parent}\n")
+            print(f"--> Valid values for RX loopback - {rx_loopback_report['Valid values']}\n")
+            print(f"==> link.RX: {link.rx} / {link.rx.parent} RX_NAME={link.rx.name} GT_NAME={link.rx.parent.name} GT_alias={link.rx.parent.aliases} ")
+            print(f"==> link.TX: {link.tx} / {link.tx.parent} TX_NAME={link.tx.name} GT_NAME={link.tx.parent.name} GT_alias={link.tx.parent.aliases} ")
+            link.generate_report()
             dbg_print = False
 
         props = {link.tx.property_for_alias[PATTERN]: "PRBS 31"}
@@ -483,6 +570,7 @@ def create_links_crossConnected():
 
 
 #------------------------------------------
+all_links = []
 if CREATE_LGROUP:
     q205 = one(ibert_gtm.gt_groups.filter_by(name="Quad_205"))
     q204 = one(ibert_gtm.gt_groups.filter_by(name="Quad_204"))
@@ -500,14 +588,19 @@ if CREATE_LGROUP:
 #------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    MainForm = MyWidget()
+    MainForm = MyWidget(len(all_links))
 
     # ## 7 - Create YK Scan
     # This step initializes the YK scan, setting its update method to the method we defined in the last step. 
-    gt_group = ibert_gtm.gt_groups.filter_by(name=QUAD_NAME)[0]
-    print(f"--> GT Group channels - {gt_group.gts}")
-    MainForm.create_YKScan_figure(gt_group, QUAD_CHAN)
-    MainForm.show()
+    if CREATE_LGROUP:
+        for link in myLinks:
+            MainForm.create_YKScan_figure(link.gt_grp, link.channel)
+    else:
+        gt_group = ibert_gtm.gt_groups.filter_by(name=QUAD_NAME)[0]
+        print(f"--> GT Group channels - {gt_group.gts}")
+        MainForm.create_YKScan_figure(gt_group, QUAD_CHAN)
+
+    MainForm.show_figures()
 
     sys.exit(app.exec_())
 
